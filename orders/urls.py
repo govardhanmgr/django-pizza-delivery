f4e888a91db830c04b38780890894cd77e33eee7
govardhanmgr <morsu.gr000@gmail.com>
2023-07-18 17:05:23 +0530

    orders crud

--- orders/urls.py
+++ orders/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 
 urlpatterns = [
-	path('',views.HelloOrdersView.as_view(), name="hello_orders"),
+	# path('',views.HelloOrdersView.as_view(), name="hello_orders"),
+	path('',views.OrderCreateListView.as_view(), name="orders"),
+    
 ]
